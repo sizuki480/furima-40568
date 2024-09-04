ef43c036fd3d3ff0fe5098aa318572a24328e357
sizuki480 <94817758+sizuki480@users.noreply.github.com>
2024-09-04 12:37:05 +0900

    画面遷移後でもデータが保持される

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,13 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to root_path
   end
 
   private
