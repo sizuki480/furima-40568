cadb464616a1d7d4f5afc6496ad5ac55bc16fd32
sizuki480 <94817758+sizuki480@users.noreply.github.com>
2024-09-05 06:57:02 +0900

    削除ボタンの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,6 +36,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
 
   private
