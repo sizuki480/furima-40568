d21d992745db7f8acefe93fa7dee92572113f0a2
sizuki480 <94817758+sizuki480@users.noreply.github.com>
2024-09-14 20:51:37 +0900

    購入済み時分岐の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :soldout, only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at desc')
@@ -57,6 +58,14 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
+
+  def soldout
+    if user_signed_in? && current_user == @item.user && !@item.buy.present?
+    else
+      redirect_to root_path
+    end
+  end
+
 end
 
 # | name             | string     | null: false |
